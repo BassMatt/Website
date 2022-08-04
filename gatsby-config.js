4780e780051c04ea0455c1d0a4099ad5b87b779c
BassMatt <bassmatt0515@gmail.com>
2022-08-04 00:20:05 -0500

    updated favicon & about page

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
           },
           {
             name: `Homepage`,
-            url: `http://localhost:8000`,
+            url: `https://bass.sh`,
           },
         ],
       },
@@ -59,9 +59,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `matthew personal website`,
+        short_name: `matthew-blog`,
+        description: `Matthew's tech and hobby blog, a list of musings`,
         start_url: `/`,
         background_color: `#fff`,
         // This will impact how browsers show your PWA/website
